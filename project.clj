78b5074d7e1f174f616ab57c675f0a850e83766e
Abner Terribili <abner.terribili@gmail.com>
2019-01-18 10:46:42 -0200

    Add gitignore

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject echo "0.1.0-SNAPSHOT"
+  :description "Echo aws lambda"
+  :url "http://example.com/FIXME"
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/data.json "0.2.6"]
+                 [com.amazonaws/aws-lambda-java-core "1.0.0"]]
+  :aot :all)
